850ce725f9086a58fc80e671235a30b1b7d334b6
Evaldo NETO <eneto@e2r6p16.42.fr>
2018-05-01 13:55:35 +0200

    fixed norm

--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/01 13:52:28 by eneto             #+#    #+#             */
+/*   Updated: 2018/05/01 13:54:57 by eneto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 # define BUFF_SIZE 5
 
-typedef struct s_file
+typedef struct	s_file
 {
-  char *buffer;
-  int fd;
-} t_file;
+	char		*buffer;
+	int			fd;
+}				t_file;
 
-int get_next_line(const int fd, char **line);
+int				get_next_line(const int fd, char **line);
 
 #endif
